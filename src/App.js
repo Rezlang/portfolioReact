52a052860d6da91fe570ee2bfd44fe43350ce676
Josh Moskoff <jmmoskoff@gmail.com>
2024-06-28 22:12:06 -0400

    updated deploy.yml and app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { MantineProvider } from '@mantine/core';
 import Home from './components/homepage';
